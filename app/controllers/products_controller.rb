e36b5ff166360b5d3c21447f85954e6d85238219
dossy <fedox.toshi@gmail.com>
2018-03-25 11:38:04 +0000

    Products table 修正+index_action定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,15 @@
 class ProductsController < ReviewController
+  
+  def index
+    @products = Product.order('id ASC').limit(12)
+  end
+  
   def show
     # productsテーブルから該当するidの作品情報を取得し@productの変数へ代入する処理を書いて下さい
     @product = Product.new # 問題3ではこのコードは消して新しくコードを書いてください
+  end
+  
+  def search ##検索フォーム
+  
   end
 end
